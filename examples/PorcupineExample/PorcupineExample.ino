451113ed7e3d6fa15d1211f79ea2abe22c03fcc4
Pouya Rostam <pouya@picovoice.ai>
2023-10-06 16:19:31 -0700

    wip

--- examples/PorcupineExample/PorcupineExample.ino
+++ examples/PorcupineExample/PorcupineExample.ino
@@ -29,6 +29,12 @@ static void wake_word_callback(void) {
     Serial.println("Wake word detected!");
 }
 
+static void print_error_message(char **message_stack, int32_t message_stack_depth) {
+    for (int32_t i = 0; i < message_stack_depth; i++) {
+        Serial.println(message_stack[i]);
+    }
+}
+
 void setup() {
 
     Serial.begin(9600);
@@ -41,6 +47,10 @@ void setup() {
         while (1);
     }
 
+    char **message_stack = NULL;
+    int32_t message_stack_depth = 0;
+    pv_status_t error_status;
+
     status = pv_porcupine_init(
         ACCESS_KEY,
         MEMORY_BUFFER_SIZE,
@@ -51,17 +61,17 @@ void setup() {
         &SENSITIVITY,
         &handle);
     if (status != PV_STATUS_SUCCESS) {
-        Serial.print("Picovoice init failed with ");
+        Serial.print("Porcupine init failed with ");
         Serial.println(pv_status_to_string(status));
-        char **message_stack = NULL;
-        int32_t message_stack_depth = 0;
-        pv_get_error_stack(
-            &message_stack,
-            &message_stack_depth);
-        for (int32_t i = 0; i < message_stack_depth; i++) {
-            Serial.println(message_stack[i]);
+
+        error_status = pv_get_error_stack(&message_stack, &message_stack_depth);
+        if (error_status != PV_STATUS_SUCCESS) {
+            Serial.println("Unable to get Porcupine error state");
+            while (1);
         }
+        print_error_message(message_stack, message_stack_depth);
         pv_free_error_stack(message_stack);
+
         while (1);
     }
     Serial.println("The board is listening for 'hey computer'...");

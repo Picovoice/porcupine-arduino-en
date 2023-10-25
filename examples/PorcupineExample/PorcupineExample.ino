6fb51c9a295f6bab7beda505f778c24006ecc234
Reza (Pouya) Rostam <42750891+mrrostam@users.noreply.github.com>
2023-10-25 11:07:55 -0700

    porcupine-arduino-en - v3.0 (#4)

--- examples/PorcupineExample/PorcupineExample.ino
+++ examples/PorcupineExample/PorcupineExample.ino
@@ -15,7 +15,7 @@
 
 #define MEMORY_BUFFER_SIZE (70 * 1024)
 
-static const char* ACCESS_KEY = "${ACCESS_KEY}"; //AccessKey string obtained from Picovoice Console (https://picovoice.ai/console/)
+static const char *ACCESS_KEY = "${ACCESS_KEY}"; // AccessKey string obtained from Picovoice Console (https://picovoice.ai/console/)
 
 static int8_t memory_buffer[MEMORY_BUFFER_SIZE] __attribute__((aligned(16)));
 
@@ -27,6 +27,12 @@ static const float SENSITIVITY = 0.75f;
 
 static void wake_word_callback(void) {
     Serial.println("Wake word detected!");
+}
+
+static void print_error_message(char **message_stack, int32_t message_stack_depth) {
+    for (int32_t i = 0; i < message_stack_depth; i++) {
+        Serial.println(message_stack[i]);
+    }
 }
 
 void setup() {
@@ -41,25 +47,37 @@ void setup() {
         while (1);
     }
 
+    char **message_stack = NULL;
+    int32_t message_stack_depth = 0;
+    pv_status_t error_status;
+
     status = pv_porcupine_init(
-            ACCESS_KEY,
-            MEMORY_BUFFER_SIZE,
-            memory_buffer,
-            1,
-            &KEYWORD_MODEL_SIZES,
-            &KEYWORD_MODELS,
-            &SENSITIVITY,
-            &handle);
+        ACCESS_KEY,
+        MEMORY_BUFFER_SIZE,
+        memory_buffer,
+        1,
+        &KEYWORD_MODEL_SIZES,
+        &KEYWORD_MODELS,
+        &SENSITIVITY,
+        &handle);
     if (status != PV_STATUS_SUCCESS) {
-        Serial.print("Picovoice init failed with ");
+        Serial.print("Porcupine init failed with ");
         Serial.println(pv_status_to_string(status));
+
+        error_status = pv_get_error_stack(&message_stack, &message_stack_depth);
+        if (error_status != PV_STATUS_SUCCESS) {
+            Serial.println("Unable to get Porcupine error state");
+            while (1);
+        }
+        print_error_message(message_stack, message_stack_depth);
+        pv_free_error_stack(message_stack);
+
         while (1);
     }
     Serial.println("The board is listening for 'hey computer'...");
 }
 
-void loop()
-{
+void loop() {
     const int16_t *buffer = pv_audio_rec_get_new_buffer();
     if (buffer) {
         int32_t keyword_index;
@@ -67,7 +85,16 @@ void loop()
         if (status != PV_STATUS_SUCCESS) {
             Serial.print("Picovoice process failed with ");
             Serial.println(pv_status_to_string(status));
-            while(1);
+            char **message_stack = NULL;
+            int32_t message_stack_depth = 0;
+            pv_get_error_stack(
+                &message_stack,
+                &message_stack_depth);
+            for (int32_t i = 0; i < message_stack_depth; i++) {
+                Serial.println(message_stack[i]);
+            }
+            pv_free_error_stack(message_stack);
+            while (1);
         }
         if (keyword_index != -1) {
             wake_word_callback();
